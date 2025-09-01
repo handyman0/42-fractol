22299631a1a445cbd2167d2a930bc7ae9af82f35
handyman0 <luiz.number@gmail.com>
2025-09-01 11:10:56 -0300

    Add basic string and memory manipulation functions to libft
    - Implemented character classification functions: ft_isalpha, ft_isascii, ft_isdigit, ft_isprint.
    - Added string manipulation functions: ft_itoa, ft_split, ft_strjoin, ft_strtrim, ft_substr, ft_strdup, ft_strmapi, ft_striteri.
    - Included memory manipulation functions: ft_memchr, ft_memcmp, ft_memcpy, ft_memmove, ft_memset.
    - Introduced linked list functions: ft_lstadd_back, ft_lstadd_front, ft_lstclear, ft_lstdelone, ft_lstiter, ft_lstlast, ft_lstmap, ft_lstnew, ft_lstsize.
    - Added output functions: ft_putchar_fd, ft_putendl_fd, ft_putnbr_fd, ft_putstr_fd.
    - Implemented string length and concatenation functions: ft_strlen, ft_strlcpy, ft_strlcat.
    - Added string comparison functions: ft_strncmp, ft_strnstr.
    - Implemented character case conversion functions: ft_tolower, ft_toupper.

---
+++ libft/src/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/13 16:06:13 by lmelo-do          #+#    #+#             */
+/*   Updated: 2025/07/24 22:18:16 by lmelo-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	node->content = content;
+	node->next = NULL;
+	return (node);
+}
