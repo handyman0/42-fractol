ce084fc9dd7183f90522df6d4a18d94442da136d
lmelo-do <lmelo-do@student.42sp.org.br>
2025-09-17 07:40:24 -0300

    Update fractol.h with new timestamp and additional color definitions

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/12 06:11:11 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/09/12 06:32:55 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/09/17 06:58:26 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,23 @@
 
 # define WIDTH 800
 # define HEIGHT 800
+
+# define BLACK 0x000000
+# define WHITE 0xFFFFFF
+# define RED 0xFF0000
+# define GREEN 0x00FF00
+# define BLUE 0x0000FF
+
+# define NEON_PINK 0xFF6EC7
+# define NEON_GREEN 0x39FF14
+# define NEON_BLUE 0x1F51FF
+# define HOT_PINK 0xFF69B4
+# define LIME 0xBFFF00
+# define ELECTRIC_PURPLE 0xBF00FF
+# define TURQUOISE 0x40E0D0
+# define GOLD 0xFFD700
+# define DEEP_SKY 0x00BFFF
+# define PSYCHEDELIC_PURPLE 0X660066
 
 typedef struct	s_complex
 {
@@ -46,8 +63,14 @@ typedef struct	s_fractal
 	void	*mlx_connection;
 	void	*mlx_window;
 	t_img	img;
+	double	escape_value;
+	int		iterations_definition;
 }				t_fractal;
 
-void	fractal_init(t_fractal *fractal);
+void		fractal_init(t_fractal *fractal);
+void		fractal_render(t_fractal *fractal);
+double		map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
+t_complex	sum_complex(t_complex z1, t_complex z2);
+t_complex	square_complex(t_complex z);
 
 #endif
